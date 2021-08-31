6c97bb2793bfb6333cde95ab6cd5036fd03884b9
violigon <vinicius@oligon.com.br>
2021-08-31 17:35:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const mongodb = require("mongodb");
-const ObjectId = mongodb.ObjectId;
 require("dotenv").config();
+
+const ObjectId = mongodb.ObjectId;
 
 (async () => {
 	const dbHost = process.env.DB_HOST;
 	const dbPort = process.env.DB_PORT;
 	const dbName = process.env.DB_NAME;
 
-	const app = express();
-	app.use(express.json());
-	const port = process.env.PORT || 3000;
 	const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+
+	console.info("Conectando ao banco de dados MongoDB...");
 
 	const options = {
 		useUnifiedTopology: true,
@@ -19,13 +19,11 @@ require("dotenv").config();
 
 	const client = await mongodb.MongoClient.connect(connectionString, options);
 
-	const db = client.db("blue_db");
-	const personagens = db.collection("personagens");
+	const app = express();
 
-	const getPersonagensValidas = () => personagens.find({}).toArray();
+	const port = process.env.PORT || 3000;
 
-	const getPersonagemById = async (id) =>
-		personagens.findOne({ _id: ObjectId(id) });
+	app.use(express.json());
 
 	app.all("/*", (req, res, next) => {
 		res.header("Access-Control-Allow-Origin", "*");
@@ -41,64 +39,134 @@ require("dotenv").config();
 	});
 
 	app.get("/", (req, res) => {
-		res.send({ info: "Olá, Blue" });
+		res.send("Olá, Blue !");
 	});
 
-	//[GET] GetAllPersonagens
+	/*
+    Lista de Endpoints da aplicação CRUD de personagens
+    CRUD: Create, Read (Single & All), Update and Delete
+    CRUD: Criar, Ler (Individual e Tudo), Atualizar e Remover
+    - [GET] /personagens - Retorna a lista de personagens
+    - [GET] /personagens/{id} - Retorna apenas uma única personagem pelo ID
+    - [POST] /personagens - Cria uma nova personagem
+    - [PUT] /personagens/{id} - Atualiza uma personagem pelo ID
+    - [DELETE] /personagens/{id} - Remover uma personagem pelo ID
+    */
 
+	const db = client.db("blue_db");
+	const personagens = db.collection("personagens");
+
+	const getpersonagensValidas = () => personagens.find({}).toArray();
+
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
+
+	// - [GET] /personagens - Retorna a lista de personagens
 	app.get("/personagens", async (req, res) => {
-		res.send(await getPersonagensValidas());
+		res.send(await getpersonagensValidas());
 	});
 
-	//[GET] getPersonagemById
-
+	// - [GET] /personagens/{id} - Retorna apenas uma única personagem pelo ID
 	app.get("/personagens/:id", async (req, res) => {
 		const id = req.params.id;
+
 		const personagem = await getPersonagemById(id);
+
+		if (!personagem) {
+			res.send("Personagem não encontrado.");
+
+			return;
+		}
+
 		res.send(personagem);
 	});
 
+	// - [POST] /personagens - Cria uma nova personagem
 	app.post("/personagens", async (req, res) => {
 		const objeto = req.body;
 
 		if (!objeto || !objeto.nome || !objeto.imagemUrl) {
-			res.send("Objeto invalido");
+			res.send("Objeto inválido.");
+
 			return;
 		}
 
-		const insertCount = await personagens.insertOne(objeto);
-		console.log(insertCount);
-		if (!insertCount) {
-			res.send("Ocorreu um erro");
+		const { insertedCount } = await personagens.insertOne(objeto);
+
+		if (insertedCount !== 1) {
+			res.send("Ocorreu um erro ao criar a personagem.");
+
 			return;
 		}
 
 		res.send(objeto);
 	});
 
+	// - [PUT] /personagens/{id} - Atualiza uma personagem pelo ID
 	app.put("/personagens/:id", async (req, res) => {
 		const id = req.params.id;
+
 		const objeto = req.body;
-		res.send(
-			await personagens.updateOne(
-				{
-					_id: ObjectId(id),
-				},
-				{
-					$set: objeto,
-				}
-			)
+
+		if (!objeto || !objeto.nome || !objeto.imagemUrl) {
+			res.send("Personagem inválido.");
+
+			return;
+		}
+
+		const quantidade_personagens = await personagens.countDocuments({
+			_id: ObjectId(id),
+		});
+
+		if (quantidade_personagens !== 1) {
+			res.send("Personagem não encontrado.");
+
+			return;
+		}
+
+		const { result } = await personagens.updateOne(
+			{
+				_id: ObjectId(id),
+			},
+			{
+				$set: objeto,
+			}
 		);
+
+		if (result.ok !== 1) {
+			res.send("Ocorreu um erro ao atualizar o personagem.");
+
+			return;
+		}
+
+		res.send(await getPersonagemById(id));
 	});
 
+	// - [DELETE] /personagens/{id} - Remover uma personagem pelo ID
 	app.delete("/personagens/:id", async (req, res) => {
 		const id = req.params.id;
 
-		res.send(
-			await personagens.deleteOne({
-				_id: ObjectId(id),
-			})
-		);
+		const quantidade_personagens = await personagens.countDocuments({
+			_id: ObjectId(id),
+		});
+
+		if (quantidade_personagens !== 1) {
+			res.send("Personagem não encontrado.");
+
+			return;
+		}
+
+		const { deletedCount } = await personagens.deleteOne({
+			_id: ObjectId(id),
+		});
+
+		if (deletedCount !== 1) {
+			res.send("Ocorreu um erro ao remover o personagem.");
+
+			return;
+		}
+
+		res.send("Personagem removido com sucesso.");
 	});
 
 	app.listen(port, () => {
